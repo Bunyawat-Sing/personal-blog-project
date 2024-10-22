88e83a03fc657351bb36b82bebd053f790ff8989
Bunyawat Singkiporn <pupahero@gmail.com>
2024-10-22 17:07:04 +0700

    fix: apply rendering list to article section

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -52,6 +52,8 @@ function BlogCard(props) {
   );
 }
 
+const categories = ["Highlight", "Cat", "Inspiration", "General"];
+
 export function ArticleSection() {
   return (
     <>
@@ -62,30 +64,16 @@ export function ArticleSection() {
 
         <div className="flex flex-col items-center justify-center md:flex-row md:justify-between gap-3 md:ml-20 md:mr-20 md:px-5 md:rounded-3xl md:h-[82px] h-[172px] bg-[#EFEEEB]">
           <div className="hidden md:flex space-x-2">
-            <Button
-              className="px-4 py-3 transition-colors rounded-sm text-sm font-medium text-[#43403B] hover:bg-[#DAD6D1] bg-[muted]]
-            "
-            >
-              Highlight
-            </Button>
-            <Button
-              className="px-4 py-3 transition-colors rounded-sm text-sm font-medium text-[#43403B] hover:bg-[#DAD6D1] bg-[muted]
-            "
-            >
-              Cat
-            </Button>
-            <Button
-              className="px-4 py-3 transition-colors rounded-sm text-sm font-medium text-[#43403B] hover:bg-[#DAD6D1] bg-[muted]
-            "
-            >
-              Inspiration
-            </Button>
-            <Button
-              className="px-4 py-3 transition-colors rounded-sm text-sm font-medium text-[#43403B] hover:bg-[#DAD6D1] bg-[muted]
-            "
-            >
-              General
-            </Button>
+            {categories.map((button) => {
+              return (
+                <Button
+                  className="px-4 py-3 transition-colors rounded-sm text-sm font-medium text-[#43403B] hover:bg-[#DAD6D1] bg-[muted]
+              "
+                >
+                  {button}
+                </Button>
+              );
+            })}
           </div>
           <div className="w-[343px] ">
             <div className="relative">
@@ -106,9 +94,9 @@ export function ArticleSection() {
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>Group</SelectLabel>
-                  <SelectItem value="item1">item1</SelectItem>
-                  <SelectItem value="item2">item2</SelectItem>
-                  <SelectItem value="item3">item3</SelectItem>
+                  {categories.map((item) => {
+                    return <SelectItem value={item}>{item}</SelectItem>;
+                  })}
                 </SelectGroup>
               </SelectContent>
             </Select>
@@ -116,54 +104,19 @@ export function ArticleSection() {
         </div>
       </div>
       <article className="font-poppins mt-10 md:mt-0 grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-0 md:ml-20 md:mr-20">
-        <BlogCard
-          image={blogPosts[0].image}
-          category={blogPosts[0].category}
-          title={blogPosts[0].title}
-          description={blogPosts[0].description}
-          author={blogPosts[0].author}
-          date={blogPosts[0].date}
-        />
-        <BlogCard
-          image={blogPosts[1].image}
-          category={blogPosts[1].category}
-          title={blogPosts[1].title}
-          description={blogPosts[1].description}
-          author={blogPosts[1].author}
-          date={blogPosts[1].date}
-        />
-        <BlogCard
-          image={blogPosts[2].image}
-          category={blogPosts[2].category}
-          title={blogPosts[2].title}
-          description={blogPosts[2].description}
-          author={blogPosts[2].author}
-          date={blogPosts[2].date}
-        />
-        <BlogCard
-          image={blogPosts[3].image}
-          category={blogPosts[3].category}
-          title={blogPosts[3].title}
-          description={blogPosts[3].description}
-          author={blogPosts[3].author}
-          date={blogPosts[3].date}
-        />
-        <BlogCard
-          image={blogPosts[4].image}
-          category={blogPosts[4].category}
-          title={blogPosts[4].title}
-          description={blogPosts[4].description}
-          author={blogPosts[4].author}
-          date={blogPosts[4].date}
-        />
-        <BlogCard
-          image={blogPosts[5].image}
-          category={blogPosts[5].category}
-          title={blogPosts[5].title}
-          description={blogPosts[5].description}
-          author={blogPosts[5].author}
-          date={blogPosts[5].date}
-        />
+        {blogPosts.map((blogProps, index) => {
+          return (
+            <BlogCard
+              key={index}
+              image={blogProps.image}
+              category={blogProps.category}
+              title={blogProps.title}
+              description={blogProps.description}
+              author={blogProps.author}
+              date={blogProps.date}
+            />
+          );
+        })}
       </article>
       <div className="font-poppins w-full mt-20 mb-20 text-center">
         <button className="underline">View More</button>
