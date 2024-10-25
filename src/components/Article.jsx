68fd43e117f7ef4dc71712c95bfdbbc05abd3203
Bunyawat Singkiporn <pupahero@gmail.com>
2024-10-25 19:27:16 +0700

    feat: add data fetching, pagination and loading for data to article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -12,7 +12,8 @@ import { Search } from "lucide-react";
 //import { Button } from "@/components/ui/button";
 import { blogPosts } from "../data/blogPosts";
 import authorImage from "../assets/img/cat.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function BlogCard(props) {
   return (
@@ -56,22 +57,73 @@ function BlogCard(props) {
 const categories = ["Highlight", "Cat", "Inspiration", "General"];
 
 export function ArticleSection() {
-  const [category, setCategory] = useState(categories[0]);
+  const [category, setCategory] = useState(categories[0]); //Button
+  const [blogPost, setBlogPost] = useState([]); //Blogpost
+  const [page, setPage] = useState(1); //Page
+  const [more, setMore] = useState(true); //Loadmore
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, [category, page]);
+
+  const fetchData = async () => {
+    /*try {
+      const endpoint =
+        category === "Highlight"
+          ? `https://blog-post-project-api.vercel.app/posts`
+          : `https://blog-post-project-api.vercel.app/posts?category=${category}`;
+      const response = await axios.get(endpoint);
+      setBlogPost(response.data.posts);
+    } catch (error) {
+      console.log(error);
+    }*/
+    setLoading(true);
+    try {
+      const endpoint =
+        category === "Highlight"
+          ? `https://blog-post-project-api.vercel.app/posts?page=${page}&limit=6`
+          : `https://blog-post-project-api.vercel.app/posts?page=${page}&limit=6&category=${category}`;
+
+      const response = await axios.get(endpoint);
+      const newPosts = response.data.posts;
+
+      if (newPosts.length < 6) {
+        setMore(false);
+      }
+
+      setBlogPost((prevPosts) =>
+        page === 1 ? newPosts : [...prevPosts, ...newPosts]
+      );
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  const loadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <>
+      {/* Article Menu Section */}
       <div className=" font-poppins h-[236px] w-screen">
         <h1 className="ml-20 text-[24px] font-semibold py-[16px] md:text-left md:ml-20">
           Lasted articles
         </h1>
-
+        {/* Article Menu Button */}
         <div className="flex flex-col items-center justify-center md:flex-row md:justify-between gap-3 md:ml-20 md:mr-20 md:px-5 md:rounded-3xl md:h-[82px] h-[172px] bg-[#EFEEEB]">
           <div className="hidden md:flex space-x-2">
             {categories.map((button) => {
               return (
                 <button
                   key={button}
-                  onClick={() => setCategory(button)}
+                  onClick={() => {
+                    setCategory(button);
+                    setPage(1);
+                    setMore(true);
+                  }}
                   className={`px-4 py-3 rounded-sm text-sm font-medium text-[#43403B] ${
                     category === button ? "bg-[#DAD6D1]" : "hover:bg-muted"
                   }`}
@@ -81,6 +133,7 @@ export function ArticleSection() {
               );
             })}
           </div>
+          {/* Article Menu Search */}
           <div className="w-[343px] ">
             <div className="relative">
               <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
@@ -91,6 +144,7 @@ export function ArticleSection() {
               />
             </div>
           </div>
+          {/* Article Menu Dropdown List */}
           <div className="md:hidden">
             <h1 className="text-[16px] font-medium text-[#75716B]">Category</h1>
             <Select>
@@ -109,24 +163,38 @@ export function ArticleSection() {
           </div>
         </div>
       </div>
+      {/* Blogcard Section */}
       <article className="font-poppins mt-10 md:mt-0 grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-0 md:ml-20 md:mr-20">
-        {blogPosts.map((blogProps, index) => {
-          return (
-            <BlogCard
-              key={index}
-              image={blogProps.image}
-              category={blogProps.category}
-              title={blogProps.title}
-              description={blogProps.description}
-              author={blogProps.author}
-              date={blogProps.date}
-            />
-          );
-        })}
+        {loading ? (
+          <div className="col-span-2 text-center">Loading...</div>
+        ) : (
+          blogPost.map((blogProps, index) => {
+            return (
+              <BlogCard
+                key={index}
+                image={blogProps.image}
+                category={blogProps.category}
+                title={blogProps.title}
+                description={blogProps.description}
+                author={blogProps.author}
+                date={new Date(blogProps.date).toLocaleDateString("en-GB", {
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                })}
+              />
+            );
+          })
+        )}
       </article>
-      <div className="font-poppins w-full mt-20 mb-20 text-center">
-        <button className="underline">View More</button>
-      </div>
+      {/* View More Button */}
+      {more && !loading && (
+        <div className="font-poppins w-full mt-20 mb-20 text-center">
+          <button onClick={loadMore} className="underline">
+            View More
+          </button>
+        </div>
+      )}
     </>
   );
 }
